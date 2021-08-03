52bdad94b173b26027f073efe2d3f7ee615caf6b
Zakhar <z.inform@mail.ru>
2021-08-03 23:27:36 +0300

    Maybe done?

---
+++ main.c
@@ -0,0 +1,19 @@
+typedef __UINT32_TYPE__ uint32_t;
+#define CLOCK_BASE 0x40021000
+#define GPIOC_BASE 0x40011000
+
+
+int main(void){
+    //init system clock BASE = 0x4002 1000
+    //HXTAL; CK_HTAL = 00; AHBpresc = 1; APBpresc = 1;
+
+    
+
+    //init clocking for GPIO_C 
+    //APB2 
+    *((uint32_t*) CLOCK_BASE + 0x18) |= 1 << 4;
+    //PC10 output   CTL = 00; MD = 10; 
+    *((uint32_t*) GPIOC_BASE + 0x04) |= 2 << 8;
+    *((uint32_t*) GPIOC_BASE + 0x0C) |= 1 << 10;
+    while(1);
+}
